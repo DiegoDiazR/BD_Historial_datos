518acd2643d56faf30725270a0f9110d175f6f29
Diego Nicolas <167242760+DiegoDiazR@users.noreply.github.com>
2026-03-25 14:17:53 -0500

    feat(voo): update

--- VOO/VOO_prices_2026_03.sql
+++ VOO/VOO_prices_2026_03.sql
@@ -2725,3 +2725,28 @@ INSERT IGNORE INTO voo_prices (
     NULL, NULL
   );
 
+INSERT IGNORE INTO voo_prices (
+    timestamp, ticker, source, price_current, price_open, price_high, price_low, 
+    price_prev_close, price_change, price_change_pct, volume, avg_volume_10d,
+    week_52_high, week_52_low, price_return_ytd, market_cap, beta
+  ) VALUES (
+    '2026-03-25 19:19:21', 'VOO', 'finnhub',
+    605.5400, 607.1250, 609.4400, 603.3600,
+    602.3800, 3.1600, 0.5246, 
+    NULL, NULL,
+    NULL, NULL, NULL, 
+    NULL, NULL
+  );
+INSERT IGNORE INTO voo_prices (
+    timestamp, ticker, source, price_current, price_open, price_high, price_low, 
+    price_prev_close, price_change, price_change_pct, volume, avg_volume_10d,
+    week_52_high, week_52_low, price_return_ytd, market_cap, beta
+  ) VALUES (
+    '2026-03-25 19:17:49', 'VOO', 'yfinance',
+    605.5550, 607.4000, 609.4300, 603.3600,
+    602.3800, 3.1750, 0.5271, 
+    5867897, NULL,
+    641.8100, 442.8000, NULL, 
+    NULL, NULL
+  );
+
